c1ce1b846290fef02d16a52f4004953b0b49c3d6
Alice Lee <alicesle@usc.edu>
2019-10-24 17:17:59 -0700

    first commit with springboot starter

---
+++ server/TutorSearcher/src/main/java/tutor/searcher/TutorSearcher/TutorSearcherApplication.java
@@ -0,0 +1,14 @@
+package tutor.searcher.TutorSearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorSearcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorSearcherApplication.class, args);
+		System.out.println("application running");
+	}
+
+}
---
+++ server/TutorSearcher/src/test/java/tutor/searcher/TutorSearcher/TutorSearcherApplicationTests.java
@@ -0,0 +1,13 @@
+package tutor.searcher.TutorSearcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorSearcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
