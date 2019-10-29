573676039f91589a5f6b5e4d2513a8ad9d22b003
Cameron Haseyama <haseyama@usc.edu>
2019-10-29 16:19:57 -0700

    Client sockets

--- TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/MainActivity.java
+++ TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.tutorsearcherandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView response;
+    EditText editTextAddress, editTextPort;
+    Button buttonConnect, buttonClear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextAddress = (EditText) findViewById(R.id.addressEditText);
+        editTextPort = (EditText) findViewById(R.id.portEditText);
+        buttonConnect = (Button) findViewById(R.id.connectButton);
+        buttonClear = (Button) findViewById(R.id.clearButton);
+        response = (TextView) findViewById(R.id.responseTextView);
+
+        buttonConnect.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                Client myClient = new Client(response);
+                myClient.execute();
+            }
+        });
+
+        buttonClear.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                response.setText("");
+            }
+        });
     }
 }
