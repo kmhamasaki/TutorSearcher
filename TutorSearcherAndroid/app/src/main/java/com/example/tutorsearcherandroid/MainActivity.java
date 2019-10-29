912790f923e690c563df2b7eece4fb8fa0a1dedd
James Aluwi <jaluwi@usc.edu>
2019-10-28 22:35:44 -0700

    add button functionality

--- TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/MainActivity.java
+++ TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/MainActivity.java
@@ -3,12 +3,31 @@ package com.example.tutorsearcherandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button1);
+        Button button2 = findViewById(R.id.button2);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button1:
+                break;
+            case R.id.button2:
+                break;
+        }
     }
 }
