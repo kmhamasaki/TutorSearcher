5fdf7a5a567d6ee37d8e6c691a349ef7309b8863
James Aluwi <jaluwi@usc.edu>
2019-10-29 15:18:37 -0700

    updated home page

--- TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/LoginActivity.java
+++ TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/LoginActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -15,6 +16,9 @@ public class LoginActivity extends AppCompatActivity {
 
         Bundle extras = getIntent().getExtras();
         String accountType = extras.getString("accountType");
+        accountType = accountType.substring(0,1).toUpperCase() + accountType.substring(1).toLowerCase();
+        TextView T =(TextView)findViewById(R.id.textView);
+        T.setText(accountType + " Log In");
         Toast.makeText(getApplicationContext(),"Acc type: "+accountType, Toast.LENGTH_SHORT).show();
     }
 }
