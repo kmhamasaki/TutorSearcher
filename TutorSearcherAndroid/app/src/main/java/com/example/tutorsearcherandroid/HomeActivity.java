c33da9c75169ae842c1232f2fcd669d04fe0001a
Alice Lee <alicesle@usc.edu>
2019-10-29 22:15:59 -0700

    added home activity

---
+++ TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/HomeActivity.java
@@ -0,0 +1,43 @@
+package com.example.tutorsearcherandroid;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onClickSearch(View view) {
+        //redirects to search screens
+    }
+
+    public void onClickUpdateProfile(View view) {
+        //redirects to update profile screen
+    }
+
+    public void onClickViewRequests(View view) {
+        //redirects to requests screen
+    }
+}
