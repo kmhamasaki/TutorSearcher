aa5c04cf3973b5e3054f9a142337aa6e16bf46a3
kmhamasaki <khamasak@usc.edu>
2019-10-29 17:12:24 -0700

    Signup page

---
+++ TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/Signup.java
@@ -0,0 +1,28 @@
+package com.example.tutorsearcherandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class Signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+
+    public void SignupButtonClick(View view) {
+        // Pass all inputs to backend
+
+        // Error if not all inputs set
+
+        // Error if email already exists
+
+        // Success, tutee, go to home page
+
+        // Success, tutor, go to availability page.
+    }
+}
+
