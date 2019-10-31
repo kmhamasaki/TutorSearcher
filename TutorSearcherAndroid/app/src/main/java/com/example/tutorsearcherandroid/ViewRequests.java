94a400212524864e927a83f49615e6857d948b45
Neuton Foo <neuton.foo@gmail.com>
2019-10-30 21:56:33 -0700

    Added template dynamic row creation

--- TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/ViewRequests.java
+++ TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/ViewRequests.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,25 +25,39 @@ public class ViewRequests extends AppCompatActivity {
         HashMap<String,Object> attributes = new HashMap<String,Object>();
         Client viewrequests = new Client("viewrequests", attributes);
 
-        List<TutorRequest> requests = viewrequests.incomingAttributes.requests
+//        List<TutorRequest> requests = viewrequests.incomingAttributes.requests
 
         TableLayout requests_table_layout = (TableLayout) findViewById(R.id.requests_table_layout);
 
+        for (int i = 0; i <2; i++) {
 
-        for(TutorRequest tutorRequest : requests) {
-            TableRow request_row = (TableRow) findViewById(R.id.request_row);
-
-            // If User == Tutor
-                // Add button to approve / reject
-            // If User == Tutee
-                // Don't add button
-
-            request_row.generateLayoutParams();
-
-            requests_table_layout.addView(request_row);
-
-
+            TableRow row = new TableRow(this);
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+            TextView tv = new TextView(this);
+            tv.setText("Testing");
+            row.addView(tv);
+            requests_table_layout.addView(row, i);
         }
+
+
+//
+//
+//
+//        for(TutorRequest tutorRequest : requests) {
+//            TableRow request_row = (TableRow) findViewById(R.id.request_row);
+//
+//            // If User == Tutor
+//                // Add button to approve / reject
+//            // If User == Tutee
+//                // Don't add button
+//
+//            request_row.generateLayoutParams();
+//
+//            requests_table_layout.addView(request_row);
+//
+//
+//        }
 
     }
 
