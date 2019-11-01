e3f45021802317030a7d3e7791567478ea6d7e9e
Cameron Haseyama <haseyama@usc.edu>
2019-10-31 18:18:45 -0700

    Create class selection intent and add more classes

--- TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/SearchTutor.java
+++ TutorSearcherAndroid/app/src/main/java/com/example/tutorsearcherandroid/SearchTutor.java
@@ -3,6 +3,7 @@ package com.example.tutorsearcherandroid;
 import androidx.appcompat.app.AppCompatActivity;
 import tutor.searcher.TutorSearcher.Request;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -12,10 +13,10 @@ import android.widget.TextView;
 
 import java.util.HashMap;
 
-public class SearchTutor extends AppCompatActivity {
+public class SearchTutor extends AppCompatActivity implements View.OnClickListener{
     TextView serverResponse;
     Spinner classSpinner;
-    Button submitButton;
+
     Request request;
 
     @Override
@@ -30,21 +31,28 @@ public class SearchTutor extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         classSpinner.setAdapter(adapter);
 
-        //Temp Response View
-        serverResponse = findViewById(R.id.responseText);
-
+        Button submitButton;
         //Submit Button
         submitButton = findViewById(R.id.submit_button);
-        submitButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                HashMap<String,Object> attributes = new HashMap<String,Object>();
+        submitButton.setOnClickListener(this);
+    }
 
-//                attributes.put("class","CSCI 103");
-//                attributes.put("time","1,10,11,20");
-                Client myClient = new Client("search", attributes);
-                myClient.execute();
-            }
-        });
+    public void onClick(View view){
+//        HashMap<String, Object> attr = new HashMap<>();
+//        attr.put("className", classSpinner.getSelectedItem().toString());
+//        attr.put("availability", "");
+//        Client client = new Client("search",attr);
+//
+//        client.execute();
+
+//        System.out.println("Adding selected class " + classSpinner.getSelectedItem().toString());
+        openAvailabilityActivity();
+    }
+
+    public void openAvailabilityActivity(){
+        Intent intent = new Intent(this, AvailabilityActivity.class);
+        intent.putExtra("CLASS_NAME", classSpinner.getSelectedItem().toString());
+        startActivity(intent);
+        finish();
     }
 }
