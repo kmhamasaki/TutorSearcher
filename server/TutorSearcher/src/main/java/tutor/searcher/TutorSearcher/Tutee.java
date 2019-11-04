9913063b432be7cc23945fba367cdc374b614578
Alice Lee <alicesle@usc.edu>
2019-11-04 14:14:01 -0800

    cleaned up backend code

--- server/TutorSearcher/src/main/java/tutor/searcher/TutorSearcher/Tutee.java
+++ server/TutorSearcher/src/main/java/tutor/searcher/TutorSearcher/Tutee.java
@@ -6,18 +6,9 @@ import java.util.List;
 public class Tutee extends User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	public Tutee(int userId, String firstName, String lastName, String passwordHash, String email, String phoneNumber, Boolean accountType, List<TutorRequest> pendingRequests,
-			List<TutorRequest> acceptedRequests, List<TutorRequest> rejectedRequests) {
-		//int userID, String firstName, String lastName, String email, String phoneNumber, String passwordHash, Boolean accountType
-		super(userId, firstName, lastName, email, phoneNumber, passwordHash, accountType, pendingRequests, acceptedRequests, rejectedRequests);
-	}
-
 	public Tutee(int userID, String firstName, String lastName, String email, String phoneNumber, Boolean accountType) {
 		// TODO Auto-generated constructor stub
 		super(userID, firstName, lastName, email, phoneNumber, accountType);
 	}
 
-	void sendRequest(TutorRequest request) {
-		return;
-	}
 }
--- server/TutorSearcher/src/test/java/tutor/searcher/TutorSearcher/DBConnectTest.java
+++ server/TutorSearcher/src/test/java/tutor/searcher/TutorSearcher/DBConnectTest.java
@@ -17,17 +17,9 @@ class DBConnectTest {
 	private Connection conn;
 	
 	public DBConnectTest() {
-//		
-//		PreparedStatement ps = null;
-//		try {
-//			conn = DriverManager.getConnection("jdbc:mysql://localhost/TutorSearcherTest?user="
-//					+ "root&password=password&useSSL=false&serverTimezone=UTC");
-//			ps = conn.prepareStatement("DELETE from users");
-//			ps.executeUpdate();
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
+
 	}
+	
 	@Test
 	void addTutorToClassTest() {
 		int tutorID = 15;
@@ -131,37 +123,6 @@ class DBConnectTest {
 		System.out.println(user.getFirstName() + " " + user.getLastName());
 	}
 	
-//	@Test
-//	void addUser() {
-//		List<Integer> avail = Arrays.asList(1, 2, 3);
-//		// Normal Insertion
-//		int UserID = dbConnect.addUser("tommy@usc.edu", "skjfakksjdg", "Tommy", "Trojan", 
-//				"8081234567", true);
-//		assertEquals(UserID, 1);
-//		
-//		// Repeat email
-//		UserID = dbConnect.addUser("tommy@usc.edu", "skjfakksjdg", "Tommy", "Trojan", 
-//				"8081234567", true);
-//		assertEquals(UserID, -1);
-//	}
-//	
-//	@Test
-//	void authenticate() {
-//		List<Integer> avail = Arrays.asList(1, 2, 3);
-//		int UserID = dbConnect.addUser("tommy@usc.edu", "skjfakksjdg", "Tommy", "Trojan", 
-//				"8081234567", true);
-//		
-//		// Correct Login
-//		User user = dbConnect.authenticate("tommy@usc.edu", "skjfakksjdg");
-//		assertTrue(user != null);
-//		
-//		// Wrong password
-//		user = dbConnect.authenticate("tommy@usc.edu", "ksjdfsa");
-//		assertTrue(user == null);
-//		
-//		// Email doesn't exist
-//		user = dbConnect.authenticate("tommy2@usc.edu", "ksjdfsa");
-//		assertTrue(user == null);
-//	}
+
 
 }
