34d578affac6086b4596e6e6c83212ac0cd3132a
Alice Lee <alicesle@usc.edu>
2019-11-03 17:28:09 -0800

    Update tutorsearcher.sql

--- server/TutorSearcher/src/main/resources/tutorsearcher.sql
+++ server/TutorSearcher/src/main/resources/tutorsearcher.sql
@@ -10,7 +10,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema tutorsearcher
 -- -----------------------------------------------------
-DROP SCHEMA IF EXISTS 'tutorsearcher';
+DROP SCHEMA IF EXISTS `tutorsearcher`;
 -- -----------------------------------------------------
 -- Schema tutorsearcher
 -- -----------------------------------------------------
