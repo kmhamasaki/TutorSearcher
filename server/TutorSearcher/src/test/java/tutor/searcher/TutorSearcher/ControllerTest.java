6d189a9293fddbfe4428853beebd1c06c82ef0f5
Neuton Foo <neuton.foo@gmail.com>
2019-11-17 15:40:15 -0800

    Socket stream testing

--- server/TutorSearcher/src/test/java/tutor/searcher/TutorSearcher/ControllerTest.java
+++ server/TutorSearcher/src/test/java/tutor/searcher/TutorSearcher/ControllerTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.HashMap;
 
@@ -13,13 +16,17 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 class ControllerTest {
     public ControllerTest() {
-
     }
 
     @Test
-    void processRequestTest_Signup() {
-        System.out.println("Testing successful signup");
+    void SignUpSuccessThread() throws InterruptedException, IOException, ClassNotFoundException {
+        // Testing duplicate sign ups
+        System.out.println("Testing successful sign ups");
 
+        // Creating new ConnectionThread
+        ControllerThread ct = new ControllerThread();
+
+        // Setting test parameters
         String email = "chunghas@usc.edu";
         String passwordHash = "0CC175B9C0F1B6A831C399E269772661";
         String firstName = "Chungha";
@@ -27,15 +34,22 @@ class ControllerTest {
         String phoneNumber = "2135052883";
         Boolean accountType = true;
 
-        Controller c = new Controller();
-
+        // DBConnect mocks
         DBConnect dbConnect = mock(DBConnect.class);
         when(dbConnect.addUser(email, passwordHash, firstName, lastName, phoneNumber, accountType)).thenReturn(1);
+        ct.setDbConnect(dbConnect);
 
-        c.setDbConnect(dbConnect);
+        // Starting Server
+        Thread thread = new Thread(ct);
+        thread.start();
+        Thread.sleep(2000);
 
-        Socket s = new Socket();
+        // Client connect
+        Socket s = new Socket("localhost", 6789);
+        ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+        ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
 
+        // Preparing Request object parameters
         String requestType = "signup";
         HashMap<String, Object> respAttr = new HashMap<String, Object>();
         respAttr.put("email", email);
@@ -45,10 +59,115 @@ class ControllerTest {
         respAttr.put("phoneNumber", phoneNumber);
         respAttr.put("accountType", accountType);
 
+        // Creating Request object
         Request request = new Request(requestType, respAttr);
+        oos.writeObject(request);
+        oos.flush();
+
+        Request res = (Request) ois.readObject();
+        System.out.println(res.getRequestType());
+    }
+
+    @Test
+    void SignUpSuccess() {
+        // Testing duplicate sign ups
+        System.out.println("Testing successful sign ups");
+
+        // Instantiating controller
+        Controller c = new Controller();
+
+        // Setting test parameters
+        String email = "chunghas@usc.edu";
+        String passwordHash = "0CC175B9C0F1B6A831C399E269772661";
+        String firstName = "Chungha";
+        String lastName = "Kim";
+        String phoneNumber = "2135052883";
+        Boolean accountType = true;
+
+        // Preparing Request object parameters
+        String requestType = "signup";
+        HashMap<String, Object> respAttr = new HashMap<String, Object>();
+        respAttr.put("email", email);
+        respAttr.put("passwordHash", passwordHash);
+        respAttr.put("firstName", firstName);
+        respAttr.put("lastName", lastName);
+        respAttr.put("phoneNumber", phoneNumber);
+        respAttr.put("accountType", accountType);
+
+        // DBConnect mocks
+        DBConnect dbConnect = mock(DBConnect.class);
+        when(dbConnect.addUser(email, passwordHash, firstName, lastName, phoneNumber, accountType)).thenReturn(1);
+//        c.setDbConnect(dbConnect);
+
+        // Creating Request object
+        Request request = new Request(requestType, respAttr);
+
+        // Mocking RequestThread object
         RequestThread requestThread = mock(RequestThread.class);
 
+        // Process Request
         Request response = c.processRequest(request, requestThread);
+
+        // Tests
         assertEquals("Success", response.getRequestType());
+        assertEquals("1", response.getAttributes().get("userID"));
+    }
+
+    @Test
+    void SignUpDuplicate() {
+        // Testing duplicate sign ups
+        System.out.println("Testing duplicate sign ups");
+
+        // Instantiating controller
+        Controller c = new Controller();
+
+        // Setting test parameters
+        String email = "chunghas@usc.edu";
+        String passwordHash = "0CC175B9C0F1B6A831C399E269772661";
+        String firstName = "Chungha";
+        String lastName = "Kim";
+        String phoneNumber = "2135052883";
+        Boolean accountType = true;
+
+        // Preparing Request object parameters
+        String requestType = "signup";
+        HashMap<String, Object> respAttr = new HashMap<String, Object>();
+        respAttr.put("email", email);
+        respAttr.put("passwordHash", passwordHash);
+        respAttr.put("firstName", firstName);
+        respAttr.put("lastName", lastName);
+        respAttr.put("phoneNumber", phoneNumber);
+        respAttr.put("accountType", accountType);
+
+        // DBConnect mocks
+        DBConnect dbConnect = mock(DBConnect.class);
+        when(dbConnect.addUser(email, passwordHash, firstName, lastName, phoneNumber, accountType)).thenReturn(-1);
+//        c.setDbConnect(dbConnect);
+
+        // Creating Request object
+        Request request = new Request(requestType, respAttr);
+
+        // Mocking RequestThread object
+        RequestThread requestThread = mock(RequestThread.class);
+
+        // Process Request
+        Request response = c.processRequest(request, requestThread);
+
+        // Tests
+        assertEquals("Error: email exists", response.getRequestType());
+        assertEquals(null, response.getAttributes().get("userID"));
+    }
+}
+
+class ControllerThread extends Controller implements Runnable {
+    public void setDbConnect(DBConnect dbConnect) {
+        this.dbConnect = dbConnect;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(" - Starting Multi-Threaded Controller -");
+
+        startController();
     }
 }
