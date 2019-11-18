81854ead761404c85d1e81108628bc9c7fb4c4e3
kmhamasaki <khamasak@usc.edu>
2019-11-17 18:12:06 -0800

    SIngup

--- TutorSearcherAndroid/app/src/androidTest/java/com/example/tutorsearcherandroid/SignupTest.java
+++ TutorSearcherAndroid/app/src/androidTest/java/com/example/tutorsearcherandroid/SignupTest.java
@@ -29,7 +29,7 @@ import static org.hamcrest.Matchers.not;
 public class SignupTest extends TestCase {
 
     @Test
-    public void SucessTest() {
+    public void SuccessTutorTest() {
         ActivityTestRule<SignupActivity> activity = new IntentsTestRule<>(SignupActivity.class, true, true);
         Intent intent = new Intent();
 
@@ -45,6 +45,27 @@ public class SignupTest extends TestCase {
 
         onView(withId(R.id.signup_button)).perform(click());
         intended(hasComponent(ChooseClasses.class.getName()));
+//        intended(hasExtra("UserId", "1"));
+        Intents.release();
+    }
+
+    @Test
+    public void SuccessTuteeTest() {
+        ActivityTestRule<SignupActivity> activity = new IntentsTestRule<>(SignupActivity.class, true, true);
+        Intent intent = new Intent();
+
+        activity.launchActivity(intent);
+        onView(withId(R.id.email)).perform(typeText("tutor2020@usc.edu"));
+        onView(withId(R.id.first_name)).perform(typeText("Success"));
+        onView(withId(R.id.last_name)).perform(typeText("Trojan"));
+        onView(withId(R.id.password)).perform(typeText("asdfas"));
+        onView(withId(R.id.phone)).perform(typeText("123456789"));
+        onView(withId(R.id.phone)).perform(ViewActions.closeSoftKeyboard());
+        onView(withId(R.id.button_tutee)).perform(click());
+        activity.getActivity().client = new ClientTest();
+
+        onView(withId(R.id.signup_button)).perform(click());
+        intended(hasComponent(ScrollingHomeActivity.class.getName()));
 //        intended(hasExtra("UserId", "1"));
         Intents.release();
     }
