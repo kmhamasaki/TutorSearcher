c15ce5a38f753172dd64d5f9e37a1c23ccdffff8
Neuton Foo <neuton.foo@gmail.com>
2019-11-17 16:19:46 -0800

    More Controller tests

--- server/TutorSearcher/src/test/java/tutor/searcher/TutorSearcher/ControllerTest.java
+++ server/TutorSearcher/src/test/java/tutor/searcher/TutorSearcher/ControllerTest.java
@@ -110,38 +110,80 @@ class ControllerTest {
         assertEquals(null, response.getAttributes().get("userID"));
     }
 
-}
+    @Test
+    void UpdateInfoThread() throws InterruptedException, IOException, ClassNotFoundException {
+        // Testing duplicate sign ups
+        System.out.println("Testing update info");
+        System.out.println();
 
-class ControllerThread extends Controller implements Runnable {
-    public void setDbConnect(DBConnect dbConnect) {
-        this.dbConnect = dbConnect;
+        // Setting test parameters
+        int userId = 1;
+        String firstName = "Chungha";
+        String lastName = "Kim";
+        String email = "chunghas@usc.edu";
+        String phoneNumber = "2135052883";
+        String passwordHash = "0CC175B9C0F1B6A831C399E269772661";
+        boolean accountType = true;
+        double rating = -1;
+
+        // Preparing Request object parameters
+        String requestType = "updateinfo";
+        HashMap<String, Object> respAttr = new HashMap<String, Object>();
+        User user = new User(userId, firstName, lastName, email, phoneNumber, accountType, rating);
+
+        // User
+        respAttr.put("user", user);
+        Request request = new Request(requestType, respAttr);
+
+        // DBConnect mocks
+        DBConnect dbConnect = mock(DBConnect.class);
+        when(dbConnect.addUser(email, passwordHash, firstName, lastName, phoneNumber, accountType)).thenReturn(-1);
+        ct.setDbConnect(dbConnect);
+
+        // Start Controller and DummyTestClient
+        DummyTestClient dc = new DummyTestClient();
+
+        // Creating Request object
+        dc.sendRequest(request);
+        Request response = dc.getResponse();
+
+        // Tests
+        assertEquals("Success", response.getRequestType());
+        assertEquals(null, response.getAttributes().get("userID"));
+
     }
 
-    @Override
-    public void run() {
-        startController();
-    }
-}
+    class ControllerThread extends Controller implements Runnable {
+        public void setDbConnect(DBConnect dbConnect) {
+            this.dbConnect = dbConnect;
+        }
 
-class DummyTestClient {
-    ObjectInputStream ois;
-    ObjectOutputStream oos;
-
-    public DummyTestClient() throws IOException {
-        System.out.println("# Starting Dummy Test Client");
-
-        Socket s = new Socket("localhost", 6789);
-        ois = new ObjectInputStream(s.getInputStream());
-        oos = new ObjectOutputStream(s.getOutputStream());
+        @Override
+        public void run() {
+            startController();
+        }
     }
 
-    public void sendRequest(Request request) throws IOException {
-        oos.writeObject(request);
-        oos.flush();
-    }
+    class DummyTestClient {
+        ObjectInputStream ois;
+        ObjectOutputStream oos;
 
-    public Request getResponse() throws IOException, ClassNotFoundException {
-        Request res = (Request) ois.readObject();
-        return res;
+        public DummyTestClient() throws IOException {
+            System.out.println("# Starting Dummy Test Client");
+
+            Socket s = new Socket("localhost", 6789);
+            ois = new ObjectInputStream(s.getInputStream());
+            oos = new ObjectOutputStream(s.getOutputStream());
+        }
+
+        public void sendRequest(Request request) throws IOException {
+            oos.writeObject(request);
+            oos.flush();
+        }
+
+        public Request getResponse() throws IOException, ClassNotFoundException {
+            Request res = (Request) ois.readObject();
+            return res;
+        }
     }
 }
